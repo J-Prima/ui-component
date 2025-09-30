bb202319a54669ed3e677433d70cd64afd510d2e
J-Prima <pandey.phathraphon@gmail.com>
2025-09-30 13:08:17 +0000

    fix issues

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as Button } from "./components/Button/Button";
-export { default as TextField } from "./components/TextField/TextField";
-export { default as Typography } from "./components/Typography/Typography";
+export { default as Button } from "./components/Button/Button.tsx";
+export { default as TextField } from "./components/TextField/TextField.tsx";
+export { default as Typography } from "./components/Typography/Typography.tsx";
