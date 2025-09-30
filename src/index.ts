764cff8e46c5af328b54c6dcaf74648f9a683b72
J-Prima <pandey.phathraphon@gmail.com>
2025-09-30 13:05:46 +0000

    fix issues

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as Button } from "./components/Button/Button";
+export { default as TextField } from "./components/TextField/TextField";
+export { default as Typography } from "./components/Typography/Typography";
